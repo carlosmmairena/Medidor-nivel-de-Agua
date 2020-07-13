0b3798b011f34fd654c40725795803e3595aa07f
Carlos Mairena <carlosmiguelmairena@gmail.com>
2020-07-12 23:55:44 -0600

    Libreria del LCD agregada

--- Medidor_Nivel_de_Agua.ino
+++ Medidor_Nivel_de_Agua.ino
@@ -15,41 +15,42 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int cm = ping(TriggerPin, EchoPin);//pasamos los datos de la variable ping como enteros a la variable cm
+
+  int cm = ping(TriggerPin, EchoPin);//pasamos los datos de la variable al metodo "ping" como enteros a la variable cm
 
   if (cm == NULL){
+
     lcd.print("Sensor no funciona");
     lcd.setCursor(0,1);
     lcd.print("Sensor failure");
     lcd.setCursor(0,0);
     Serial.println("El sensor ultrasónico no funciona...");  
-  }
-  else
-  if (cm < 5)  {
+
+  } else if (cm < 5)  {
+
     lcd.print("Tanque lleno :)");
     lcd.setCursor(0,1);
     lcd.print("Full tank :) ");
     lcd.setCursor(0,0);
     Serial.println("Tanque lleno :)");
-  }
-  else
-  if (cm < 10) {
+
+  } else if (cm < 10) {
+
     lcd.print("Hay menos de 90% de agua ");
     lcd.setCursor(0,1);
     lcd.print("Less than 90% ");
     lcd.setCursor(0,0);
     Serial.println("Hay menos de 90% de agua");
-  }
-  else
-  if (cm < 20) {
+
+  } else if (cm < 20) {
+
     lcd.print("Hay menos de 50% de agua ");
     lcd.setCursor(0,1);
     lcd.print("Less than 50% ");
     lcd.setCursor(0,0);
     Serial.println("Hay menos de 50% de agua");
-  }
-  else {
+
+  } else {
     lcd.print("Vacio: 0%  :(   ");
     lcd.setCursor(0,1);
     lcd.print("Empty: 0% :(  ");
@@ -61,6 +62,7 @@ void loop() {
   Serial.println(cm);
   delay(1000);
 }
+// Metodo que retorna la distancia calculada de acuerdo a los valores recibidos
   int ping(int TriggerPin, int EchoPin) {
     
   digitalWrite(TriggerPin, LOW);  //para generar un pulso limpio ponemos a LOW 4us
